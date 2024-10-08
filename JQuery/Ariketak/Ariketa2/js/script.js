cb9452e8a08bb6f0d2632b2f384c315c0f295785
Gorka Gacia <garcia.gorka@uni.eus>
2024-10-08 10:52:31 +0200

    Develop JS2 rocket exercise improvement

--- JQuery/Ariketak/Ariketa2/js/script.js
+++ JQuery/Ariketak/Ariketa2/js/script.js
@@ -1,18 +1,103 @@
-$(document).ready(function () {
-  $(document).on("keydown", function (e) {
-    switch (e.key) {
-      case "ArrowLeft":
-        $("#rocket").animate({ left: "-=10px" }, "fast");
-        break;
-      case "ArrowDown":
-        $("#rocket").animate({ top: "+=10px" }, "fast");
-        break;
-      case "ArrowRight":
-        $("#rocket").animate({ left: "+=10px" }, "fast");
-        break;
-      case "ArrowUp":
-        $("#rocket").animate({ top: "-=10px" }, "fast");
-        break;
-    }
+$(function () {
+  $(document).on('keydown', function (e) {
+      console.log(e.key);
+      switch (e.key) {
+          case "ArrowLeft":
+              $('#rocket').css({'transform':'rotate(0deg)'});
+              if (e.ctrlKey) {
+                  $('#rocket')
+                      .css({'transform':'rotate(-45deg)'})
+                      .animate({
+                          top: "-=10px",
+                          left: "-=10px"
+                      }, 50);
+              } else if (e.shiftKey) {
+                  $('#rocket')
+                      .css({'transform':'rotate(-135deg)'})
+                      .animate({
+                          top: "+=10px",
+                          left: "-=10px"
+                      }, 50);
+              } else {
+                  $('#rocket')
+                      .css({'transform':'rotate(-90deg)'})
+                      .animate({
+                          left: "-=10px"
+                      }, 50);
+              }
+              break;
+          case "ArrowRight":
+              $('#rocket').css({'transform':'rotate(0deg)'});
+              if (e.ctrlKey) {
+                  $('#rocket')
+                      .css({'transform':'rotate(45deg)'})
+                      .animate({
+                          top: "-=10px",
+                          left: "+=10px"
+                      }, 50);
+              } else if (e.shiftKey) {
+                  $('#rocket')
+                      .css({'transform':'rotate(135deg)'})
+                      .animate({
+                          top: "+=10px",
+                          left: "+=10px"
+                      }, 50);
+              } else {
+                  $('#rocket')
+                      .css({'transform':'rotate(90deg)'})
+                      .animate({
+                          left: "+=10px"
+                      }, 50);
+              }
+              break;
+          case "ArrowUp":
+              $('#rocket').css({'transform':'rotate(0deg)'});
+              if (e.ctrlKey) {
+                  $('#rocket')
+                      .css({'transform':'rotate(45deg)'})
+                      .animate({
+                          top: "-=10px",
+                          left: "+=10px"
+                      }, 50);
+              } else if (e.shiftKey) {
+                  $('#rocket')
+                      .css({'transform':'rotate(-45deg)'})
+                      .animate({
+                          top: "-=10px",
+                          left: "-=10px"
+                      }, 50);
+              } else {
+                  $('#rocket')
+                      .css({'transform':'rotate(0deg)'})
+                      .animate({
+                          top: "-=10px"
+                      }, 50);
+              }
+              break;
+          case "ArrowDown":
+              $('#rocket').css({'transform':'rotate(0deg)'});
+              if (e.ctrlKey) {
+                  $('#rocket')
+                      .css({'transform':'rotate(135deg)'})
+                      .animate({
+                          top: "+=10px",
+                          left: "+=10px"
+                      }, 50);
+              } else if (e.shiftKey) {
+                  $('#rocket')
+                      .css({'transform':'rotate(-135deg)'})
+                      .animate({
+                          top: "+=10px",
+                          left: "-=10px"
+                      }, 50);
+              } else {
+                  $('#rocket')
+                      .css({'transform':'rotate(180deg)'})
+                      .animate({
+                          top: "+=10px"
+                      }, 50);
+              }
+              break;
+      }
   });
 });
